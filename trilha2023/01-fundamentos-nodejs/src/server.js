aefa4062cd586c13125868574f01c45d0db0315f
jhonnydevbr <jhonnybrustolin@gmail.com>
2023-01-13 19:34:27 -0300

    Aprendendo e adicionando ma aplicação os HTTP status codes

--- trilha2023/01-fundamentos-nodejs/src/server.js
+++ trilha2023/01-fundamentos-nodejs/src/server.js
@@ -20,9 +20,11 @@ const server = http.createServer((req, res)=>{
             email: 'johndoe@example.com'
         })
 
+        return res.writeHead(201).end()
+
     }
 
-    return res.end('Hello World')
+    return res.writeHead(404).end()
 });
 
 server.listen(3333);
