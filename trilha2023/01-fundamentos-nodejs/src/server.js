48baec201603b6a55371f23fc5bedf383278046e
jhonnydevbr <jhonnybrustolin@gmail.com>
2023-01-15 16:42:09 -0300

    Separando as rotas da aplicação

--- trilha2023/01-fundamentos-nodejs/src/server.js
+++ trilha2023/01-fundamentos-nodejs/src/server.js
@@ -1,9 +1,8 @@
 import http from 'node:http';
-import { randomUUID } from 'node:crypto';
-import { Database } from './databate.js';
 import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
 
-const database = new Database()
+
 
 const server = http.createServer(async (req, res)=>{
 
@@ -11,31 +10,12 @@ const server = http.createServer(async (req, res)=>{
 
     await json(req,res)
 
-    console.log(req.body)
+    const route = routes.find(route =>{
+        return route.method == method && route.path == url
+    })
 
-
-    if (method == 'GET' && url == '/users'){
-
-        const users = database.select('users')
-
-        return res
-        .end(JSON.stringify(users))
-    }
-
-    if (method == 'POST' && url == '/users'){
-        
-        const { name, email } = req.body
-
-        const user = {
-            id: randomUUID(),
-            name,
-            email
-        }
-
-        database.insert('users', user)
-        
-        return res.writeHead(201).end()
-
+    if(route){
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end()
