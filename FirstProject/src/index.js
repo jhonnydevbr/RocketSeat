3c47840d4e36121d6d62900182d92d97d5fdc8e0
jhonnydevbr <jhonnybrustolin@gmail.com>
2023-01-22 00:20:25 -0300

    Finalizando aplicação adicionando a funcionalidade de excluir conta e ver balanço total

--- FirstProject/src/index.js
+++ FirstProject/src/index.js
@@ -127,5 +127,22 @@ app.put("/account", verifyExistsAccountCPF, (req, res)=>{
 app.get("/account", verifyExistsAccountCPF,  (req, res)=>{
     const { customer } = req;
     return res.json(customer)
+});
+
+app.delete("/account", verifyExistsAccountCPF, (req, res)=>{
+    const { customer } = req;
+
+    customers.splice(customer, 1);
+
+    return res.status(200).json(customers)
+});
+
+app.get("/balance", (req, res)=>{
+    const { customer } = req;
+
+    const balance = getBalance(customer.statement);
+
+    return res.json(balance);
 })
+
 app.listen(3333);
