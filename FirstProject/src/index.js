3776c084a436e804fe5d4571d3d7dc5263a1d85f
jhonnydevbr <jhonnybrustolin@gmail.com>
2023-01-22 00:11:02 -0300

    Listando extrato bancário por data

--- FirstProject/src/index.js
+++ FirstProject/src/index.js
@@ -102,4 +102,17 @@ app.post("/withdraw", verifyExistsAccountCPF, (req, res)=>{
     return res.status(201).send();
 })
 
+app.get("/statement/date", verifyExistsAccountCPF, (req, res) => {
+
+    const { customer } = req;
+    const { date } = req.query;
+
+
+    const dateFormat = new Date(date + "00:00");
+
+    const statement = customer.statement.filter((statement)=> statement.created_at.toDateString() === new Date(dateFormat).toDateString)
+
+    return res.json(customer.statement);
+});
+
 app.listen(3333);
