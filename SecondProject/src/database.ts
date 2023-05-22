1ba7468c09ff8b25cfa354bec26b7e23f0c6d53a
jhonnydevbr <jhonnybrustolin@gmail.com>
2023-05-22 17:13:18 -0300

    teste

--- SecondProject/src/database.ts
+++ SecondProject/src/database.ts
@@ -8,7 +8,7 @@ export const config: Knex.Config = {
 
     useNullAsDefault: true,
     migrations:{
-        extension: 'ts',
+        extension: 'ts',asa
         directory: './db/migrations',
     },
 }
